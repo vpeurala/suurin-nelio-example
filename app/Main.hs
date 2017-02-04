e8f62dbcfedf923fed4ee88dec1a665f7ec8c680
Ville Peurala <ville.peurala@gmail.com>
2017-02-04 13:50:47 +0200

    WIP. Crashes to an undefined.

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,11 @@ import Lib
 main :: IO ()
 main = do
   randomBars <- generate100RandomBars
-  let biggestSquare :: [Int]
-      biggestSquare = findBiggestSquare randomBars
+  let biggestSquare :: Int
+      biggestSquare = findBiggestSquareSize randomBars
+  putStrLn $ show randomBars
   putStrLn (showRowOfBars randomBars)
+  putStrLn (show biggestSquare)
   --putStrLn (showRowOfBars biggestSquare)
 
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib
     (
-     findBiggestSquare
+     findBiggestSquareSize
    , generate100RandomBars
    , showRowOfBars
     ) where
@@ -13,10 +13,37 @@ generate100RandomBars :: IO [Int]
 generate100RandomBars =
   replicateM 100 (getStdRandom (randomR (1 :: Int, 6)))
 
-findBiggestSquare :: [Int] -> [Int]
-findBiggestSquare = id
+-- The width and height (which are the same number) of a square,
+-- represented as a single number here.
+type SquareSize = Int
 
-showRowOfBars :: [Int] -> String
+type Bar = Int
+
+findBiggestSquareSize :: [Bar] -> SquareSize
+findBiggestSquareSize bars =
+  let minBar = minimum bars
+      maxBar = maximum bars
+      length' = length bars
+      -- The size of square we start looking for first is the
+      -- minimum of the largest bar and the length, since
+      -- a square contained in a rectangle with width = length'
+      -- and height = maxBar cannot be larger than the smaller of
+      -- the dimensions.
+      startingAttempt = max maxBar length'
+  in  iterativelyFindBiggestSquareSize bars startingAttempt
+
+iterativelyFindBiggestSquareSize :: [Bar] -> SquareSize -> SquareSize
+iterativelyFindBiggestSquareSize _ 1 = 1
+iterativelyFindBiggestSquareSize bars attemptedSize =
+  if (containsSquareOfSize bars attemptedSize)
+  then attemptedSize
+  else iterativelyFindBiggestSquareSize bars (attemptedSize - 1)
+
+containsSquareOfSize :: [Bar] -> SquareSize -> Bool
+containsSquareOfSize _ 1 = True
+containsSquareOfSize bars size = undefined
+
+showRowOfBars :: [Bar] -> String
 showRowOfBars bars =
   let minBar = minimum bars
       maxBar = maximum bars
@@ -25,7 +52,7 @@ showRowOfBars bars =
       meter :: [String]
       meter = fmap (\height -> row height) range
       row :: Int -> String
-      row height = fmap (\bar -> mark bar height) range
+      row height = fmap (\bar -> mark bar height) bars
       mark :: Int -> Int -> Char
       mark bar height = if (bar >= height) then '*' else ' '
   in intercalate "\n" meter
